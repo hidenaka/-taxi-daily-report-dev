c8e2bddd9e7d47c1d37456a0d5fb1717a09de3ca
macbookair <65018254+hidenaka@users.noreply.github.com>
2026-05-20 18:29:50 +0900

    Revert "feat(summary-drive): 合計金額のみで日報登録モード (decisions 8 / sw v166)"
    This reverts commit c925e98d35eec43498426bfa8055abaf4ab53d35.

--- sw.js
+++ sw.js
@@ -34,7 +34,6 @@ const STATIC_FILES = [
   './js/invite-url.js',
   './js/slug-gen.js',
   './js/qr-code.js',
-  './js/summary-drive.js',
   './js/aggregate-access.js',
   './js/help-toggle.js',
   './js/legal-footer.js',
